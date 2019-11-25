602a0ca24cebad3f5b2cb90ecb76a1beea7cc56f
fablefebvre <fab.lefebvre@gmail.com>
2019-11-25 14:36:40 +0100

    exo 5 et 6

--- src/exo06.js
+++ src/exo06.js
@@ -1,18 +1,20 @@
+import { arrayExpression } from "@babel/types";
+
 export function parseUserData(data) {
   // Résoudre l'exercice en utilisant uniquement des opérateurs, pas de if()
 
   return {
     // retourner data.name ou "Anonymous" si data.name est falsey
-    name: data.name,
+    name: data.name ? data.name : "Anonymous",
 
     // convertir data.age en nombre, retourner 0 si ce n'est pas un nombre
-    age: data.age,
+    age: +data.age,
 
     // retourner true si data.rightId est égal à 0
-    isAdmin: false,
+    isAdmin: data.rightId === 0,
 
     // si data.friends est une Array, retourner sa longueur
     // sinon retourner false
-    friends: 0
+    friends: Array.isArray(data.friends) ? data.friends.length : false
   };
 }
