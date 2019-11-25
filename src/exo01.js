07b04828ec61f981b885b0158c20544f5be57fad
fablefebvre <fab.lefebvre@gmail.com>
2019-11-25 11:08:49 +0100

    exo1 et exo3

--- src/exo01.js
+++ src/exo01.js
@@ -2,14 +2,8 @@ import { booleanLiteral } from "@babel/types";
 
 // retourne true si la variable passée est une primitive
 export function isPrimitive(x) {
-    if(x typeof undefined ||
-        x typeof boolean ||
-        x typeof number ||
-        x typeof string ||
-        x typeof symbol
-        ) {
-        return true;
-    } else {
-        return false;
-    }
+  return (
+    x === null ||
+    ["undefined", "boolean", "number", "string", "symbol"].includes(typeof x)
+  );
 }
--- src/exo03.js
+++ src/exo03.js
@@ -1,6 +1,12 @@
+import { objectExpression } from "@babel/types";
+
 // retourne un objet où les valeurs des propriétés sont devenues les clés et les clés les valeurs
 // { a: "b" } => { b: "a" }
 
 export function invertKeysAndValues(obj) {
-  return obj;
+  let reversed = {};
+  for (let key in obj) {
+    reversed[obj[key]] = key;
+  }
+  return reversed;
 }
