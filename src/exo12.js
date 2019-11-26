eb25ae3808020c56b54f1deac339e6779cdf8299
fablefebvre <fab.lefebvre@gmail.com>
2019-11-26 11:38:18 +0100

    exo12

--- src/exo12.js
+++ src/exo12.js
@@ -1,15 +1,27 @@
 export function PubSub() {
-  this.events = new Map()
+  this.events = new Map();
 }
 
 PubSub.prototype.on = function(event, callback) {
   // TODO: enregistrer le callback à déclencher suite à l'événement `event`
+  if (!this.events.has(event)) {
+    this.events.set(event, []);
+  }
+  if (event === "*") {
+    this.events.values(e => e.push(callback));
+  } else {
+    this.events.get(event).push(callback);
+  }
 };
 
 PubSub.prototype.emit = function(event, data) {
   // TODO: appeler les callbacks enregistrés pour l'événement `event`
+  this.events.get(event).map(callback => callback(data));
 };
 
+PubSub.prototype.off = function(event, callback) {
+  this.events.set(event, this.events.get(event).filter(c => c !== callback));
+};
 // Pour aller plus loin:
 // - méthode off(event, callback) pour retirer une souscription
 // - on('*') pour réagir à tous les événements
