c63d588108f432092f524048def9d791c71066c8
fablefebvre <fab.lefebvre@gmail.com>
2019-11-26 11:43:32 +0100

    exo11

--- src/exo11.js
+++ src/exo11.js
@@ -1,13 +1,17 @@
 export function deduplicateArray(arr) {
   // TODO: retirer les valeurs en double dans l'array et retourner l'array filtrée
+
   // 1 - avec les méthodes filter et indexOf
+  //arr = arr.filter((value, index, arr) => arr.indexOf(value) === index );
   // 2 - avec un Set et l'opérateur spread
-  return arr;
+  return [...new Set(arr)];
 }
 
 export function getPropertyFromValue(obj, val) {
   // TODO: retourner la clé de la propriété de obj ayant comme valeur val
   // 1 - avec les méthodes find et Object.keys
+  //return Object.keys(obj).find((value, index,arr) =>
+  //          obj[value] === val);
   // 2 - avec une Map et Object.entries
-  return null;
+  return new Map(Object.entries(obj).map(item => item.reverse())).get(val);
 }
